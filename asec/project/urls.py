d01d0e08172f08adb8f25dd73650b3e6abb8a1c9
Shaimaa-moh <93445956+Shaimaa-moh@users.noreply.github.com>
2022-10-30 22:25:45 +0200

    Delete asec directory

--- asec/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from graphviz import view
-from . import views
-url_patterns =[
-    
-    path("",views.index,name="index"),
-    path("<str:name>",views.greeting,name="greeting")
-]
